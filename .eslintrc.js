a19d70f4ecf2dfd5d98914fe27b172fb324e1795
Onder Bakirtas <onderbakirtas@gmail.com>
2021-01-24 16:15:29 +0300

    eslint fixes for deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'offf' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-unused-vars': 0,
     semi: 0,
     'comma-dangle': 0
